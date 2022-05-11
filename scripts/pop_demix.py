5028d8a8b43bd984a8e1365e03ce67ebfcca72f4
Restyled.io <commits@restyled.io>
2022-05-11 16:44:09 +0000

    Restyled by autopep8

--- scripts/pop_demix.py
+++ scripts/pop_demix.py
@@ -9,12 +9,14 @@ print(
         )
     )
     + keys_used(
-        Demix.populate(display_progress=False, processes=1024, reserve_jobs=True)
+        Demix.populate(display_progress=False,
+                       processes=1024, reserve_jobs=True)
     )
     + keys_used(
         Cosine.populate(reserve_jobs=True, display_progress=False, processes=1)
     )
     + keys_used(
-        SpikeSNR.populate(reserve_jobs=True, display_progress=False, processes=1)
+        SpikeSNR.populate(reserve_jobs=True,
+                          display_progress=False, processes=1)
     ),
 )
--- scripts/pop_fields.py
+++ scripts/pop_fields.py
@@ -4,9 +4,11 @@ from scripts.helper import keys_used
 print(
     "keys processed:",
     keys_used(
-        EField.populate(reserve_jobs=True, processes=1024, display_progress=False)
+        EField.populate(reserve_jobs=True, processes=1024,
+                        display_progress=False)
     )
     + keys_used(
-        DField.populate(reserve_jobs=True, processes=1024, display_progress=False)
+        DField.populate(reserve_jobs=True, processes=1024,
+                        display_progress=False)
     ),
 )
